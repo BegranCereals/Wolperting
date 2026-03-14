bc781c484002a65caacd87a43c01b06226debb38
BegranCereals <martin.halbach@gmx.de>
2026-03-14 17:39:45 +0100

    logic.py aktualisieren

--- logic.py
+++ logic.py
@@ -1,36 +1,25 @@
 import json
 
 class Character:
-    def __init__(self, name="Neuer Held"):
-        self.name = name
-        # Grundwerte (können später per PDF erweitert werden)
+    def __init__(self):
+        self.name = "Neuer Held"
+        self.class_name = "Abenteurer"
+        self.level = 1
+        self.race = "Unbekannt"
+        self.hp_max = 10
+        self.hp_current = 10
+        self.ac = 10
+        self.initiative = 0
         self.stats = {
-            "Stärke": 10,
-            "Geschick": 10,
-            "Konstitution": 10,
-            "Intelligenz": 10,
-            "Weisheit": 10,
-            "Charisma": 10
+            "Stärke": 10, "Geschicklichkeit": 10, "Konstitution": 10,
+            "Intelligenz": 10, "Weisheit": 10, "Charisma": 10
         }
-        self.compendium_entries = [] # Hier landen später PDF-Extrakte
-
-    def get_modifier(self, stat_name):
-        value = self.stats.get(stat_name, 10)
-        return (value - 10) // 2
 
     def to_dict(self):
-        """Wandelt das Objekt in ein Dictionary für JSON um"""
-        return {
-            "name": self.name,
-            "stats": self.stats,
-            "entries": self.compendium_entries
-        }
+        return self.__dict__
 
     @classmethod
     def from_dict(cls, data):
-        """Erstellt ein Objekt aus einem Dictionary"""
-        char = cls(data.get("name", "Unbekannt"))
-        char.stats = data.get("stats", char.stats)
-        char.compendium_entries = data.get("entries", [])
+        char = cls()
+        char.__dict__.update(data)
         return char
-
